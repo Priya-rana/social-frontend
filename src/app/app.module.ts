65521aa214e9667a11ef9d1466ef3c2490a70ac4
sourabh.grover <Sourabh.Grover@kibocommerce.com>
2018-04-12 18:23:39 +0530

    Dashboard page setup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,21 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { PlatformComponent } from './platform/platform.component';
+import { LeftNavBarComponent } from './left-nav-bar/left-nav-bar.component';
+import { FooterComponent } from './footer/footer.component';
+import { RightSidebarComponent } from './right-sidebar/right-sidebar.component';
+import { NavHeaderComponent } from './nav-header/nav-header.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PlatformComponent,
+    LeftNavBarComponent,
+    FooterComponent,
+    RightSidebarComponent,
+    NavHeaderComponent
   ],
   imports: [
     BrowserModule
