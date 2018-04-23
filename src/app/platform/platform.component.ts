201dd0f8377a1a5d808615a79dab2704fc059f03
sourabh.grover <Sourabh.Grover@kibocommerce.com>
2018-04-23 19:25:52 +0530

    Temp push to handle image upload

--- src/app/platform/platform.component.ts
+++ src/app/platform/platform.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit ,ElementRef } from '@angular/core';
 import {PlatformService} from '../services/platform.service';
 // For Getting Route Parameters
 import { ActivatedRoute,Router } from '@angular/router';
@@ -12,7 +12,7 @@ export class PlatformComponent implements OnInit {
   platforms;
   url;
 
-  constructor(private route: ActivatedRoute,private router: Router,private service: PlatformService) { 
+  constructor(private route: ActivatedRoute,private router: Router,private service: PlatformService,private el: ElementRef) { 
     
   }
 
@@ -35,14 +35,37 @@ export class PlatformComponent implements OnInit {
   }
 
   createPlatform(postData){
-    console.log(postData);
+    console.log(postData.value.photo = 'asasa');
+    //locate the file element meant for the file upload.
+    let inputEl: HTMLInputElement = this.el.nativeElement.querySelector('#photo');
+    //get the total amount of files attached to the file input.
+        let fileCount: number = inputEl.files.length;
+    //create a new fromdata instance
+        let formData = new FormData();
+    //check if the filecount is greater than zero, to be sure a file was selected.
+        if (fileCount > 0) { // a file was selected
+            //append the key name 'photo' with the first file in the element
+                formData.append('photo', inputEl.files.item(0));
+                postData.value.photo = inputEl.files.item(0);
+            //call the angular http method
+            this.service.post(postData.value).subscribe(
+              (success) => {
+                      console.log(success);
+             },
+             (error) => console.log(error));
+                
+          }
+          formData.append('field', 'value');
+          console.log(postData); 
+       
+    // console.log(formData);
     // If there is no error in posted data 
-    if(postData.valid){
-      // Send Request To Server
-      this.service.post(postData.value).subscribe(platformObject => {
-        console.log(platformObject);
-      });
-    }
+    // if(postData.valid){
+    //   // Send Request To Server
+    //   this.service.post(postData.value).subscribe(platformObject => {
+    //     console.log(platformObject);
+    //   });
+    // }
     
   }
 
